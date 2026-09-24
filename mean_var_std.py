5d75acdfbffd832cb53d19642912507448c08604
nnithyasree44-svg <nnithyasree44@jnn.edu.in>
2026-09-24 15:12:12 +0000

    Complete mean variance standard deviation calculator

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,45 @@
 import numpy as np
 
+
 def calculate(list):
+    # Check whether the list contains exactly 9 numbers
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    # Convert list into 3 x 3 NumPy array
+    matrix = np.array(list).reshape(3, 3)
 
-
+    calculations = {
+        'mean': [
+            np.mean(matrix, axis=0).tolist(),
+            np.mean(matrix, axis=1).tolist(),
+            np.mean(matrix).tolist()
+        ],
+        'variance': [
+            np.var(matrix, axis=0).tolist(),
+            np.var(matrix, axis=1).tolist(),
+            np.var(matrix).tolist()
+        ],
+        'standard deviation': [
+            np.std(matrix, axis=0).tolist(),
+            np.std(matrix, axis=1).tolist(),
+            np.std(matrix).tolist()
+        ],
+        'max': [
+            np.max(matrix, axis=0).tolist(),
+            np.max(matrix, axis=1).tolist(),
+            np.max(matrix).tolist()
+        ],
+        'min': [
+            np.min(matrix, axis=0).tolist(),
+            np.min(matrix, axis=1).tolist(),
+            np.min(matrix).tolist()
+        ],
+        'sum': [
+            np.sum(matrix, axis=0).tolist(),
+            np.sum(matrix, axis=1).tolist(),
+            np.sum(matrix).tolist()
+        ]
+    }
 
     return calculations
